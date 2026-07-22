d79d6763718dc33e5f737b877ca4b33fede59e8f
orionowell-art <orionowell@gmail.com>
2026-07-22 22:18:07 +0000

    Completed Assignment 6: JavaScript data types and console logging

--- src/script.js
+++ src/script.js
@@ -5,3 +5,43 @@ let email = prompt("Enter your email address:")
 
 //Display the information
 alert(`Hi ${name}, you are ${age} years old and we will keep in touch via your email: ${email}`)
+
+//Storing my name inside a variable and looging it to the console
+let myname = "Patience";
+console.log(myname)
+
+//Storing my age inside a variable
+let myage = 26
+console.log(myage)
+
+//adding two numbers
+let addition = 19 + 7
+console.log(addition)
+
+//Subtracting two numbers
+let subtraction = 19 - 7
+console.log(subtraction)
+
+//Dividing two numbers
+let division = 19 / 7
+console.log(division)
+
+//Multiplying two numbers
+let multiplication = 19 * 7
+console.log(multiplication)
+
+//Finding the remainder of 15 % 4
+let remainder = 15 % 4
+console.log(remainder)
+
+//Storing a boolean
+let isCrochetstore = true
+console.log(isCrochetstore)
+
+//Checking the type of a number
+let price = 50.00
+console.log(typeof price)
+
+//Checking the type of string
+let crochetitem = "Bikini"
+console.log(typeof crochetitem)
